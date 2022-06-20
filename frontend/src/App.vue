eeb9d2c0194cd5a050a8188bbf3812400be0ef33
toinoulemoche <avaurs28@gmail.com>
2022-06-20 16:04:37 +0200

    OK: Implémentation des routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,36 @@
 <script setup>
 import { useRouter, useRoute } from 'vue-router'
 
-export default {
-  setup() {
-    const router = useRouter()
-    const route = useRoute()
-    router.push({name: 'login', name: 'register'})
+const router = VueRouter.createRouter({
+history: VueRouter.createWebHistory(),
+routes: [
+    {
+    path: '/',
+    name: 'Home',
+    component: Home
+    },
+    {
+    path: '/about',
+    name: 'About',
+    component: About
+    },
+    {
+    path: '/login',
+    name: 'Login',
+    component: Login
+    },
+    {
+    path: '/register',
+    name: 'Register',
+    component: Register
+    },
+    {
+    path: '/todo',
+    name: 'Todo',
+    component: Todo
     }
-}
+  ]
+})
 
 </script>
 
